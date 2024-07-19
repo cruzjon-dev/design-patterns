a2063ac8950c1f590e10da1c4faeb4d52e1ce8de
Jonathan Cruz <cruzjon.dev@gmail.com>
2024-07-19 11:20:40 -0600

    Add Memento pattern example.

---
+++ behavioral/memento.php
@@ -0,0 +1,198 @@
+<?php
+
+/****************************************************************
+ * MEMENTO
+ * - Also known as: Snapshot.
+ * - A behavioral design pattern that allows making snapshots of an object’s state and restoring it in future.
+ * - The Memento pattern delegates creating the state snapshots to the actual owner of that state, the originator object.
+ * - The pattern suggests storing the copy of the object’s state in a special object called memento. The contents of the memento aren’t accessible to any other object except the one that produced it. Other objects must communicate with mementos using a limited interface which may allow fetching the snapshot’s metadata (creation time, the name of the performed operation, etc.), but not the original object’s state contained in the snapshot.
+****************************************************************/
+
+
+/**
+ * The Originator holds some important state that may change over time. It also
+ * defines a method for saving the state inside a memento and another method for
+ * restoring the state from it.
+ */
+class Originator
+{
+    /**
+     * @var string For the sake of simplicity, the originator's state is stored
+     * inside a single variable.
+     */
+    private $state;
+
+    public function __construct(string $state)
+    {
+        $this->state = $state;
+        echo "Originator: My initial state is: {$this->state}<br/>";
+    }
+
+    /**
+     * The Originator's business logic may affect its internal state. Therefore,
+     * the client should backup the state before launching methods of the
+     * business logic via the save() method.
+     */
+    public function doSomething(): void
+    {
+        echo "Originator: I'm doing something important.<br/>";
+        $this->state = $this->generateRandomString(30);
+        echo "Originator: and my state has changed to: {$this->state}<br/>";
+    }
+
+    private function generateRandomString(int $length = 10): string
+    {
+        return substr(
+            str_shuffle(
+                str_repeat(
+                    $x = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                    ceil($length / strlen($x))
+                )
+            ),
+            1,
+            $length,
+        );
+    }
+
+    /**
+     * Saves the current state inside a memento.
+     */
+    public function save(): Memento
+    {
+        return new ConcreteMemento($this->state);
+    }
+
+    /**
+     * Restores the Originator's state from a memento object.
+     */
+    public function restore(Memento $memento): void
+    {
+        $this->state = $memento->getState();
+        echo "Originator: My state has changed to: {$this->state}<br/>";
+    }
+}
+
+/**
+ * The Memento interface provides a way to retrieve the memento's metadata, such
+ * as creation date or name. However, it doesn't expose the Originator's state.
+ */
+interface Memento
+{
+    public function getName(): string;
+
+    public function getDate(): string;
+}
+
+/**
+ * The Concrete Memento contains the infrastructure for storing the Originator's
+ * state.
+ */
+class ConcreteMemento implements Memento
+{
+    private $state;
+
+    private $date;
+
+    public function __construct(string $state)
+    {
+        $this->state = $state;
+        $this->date = date('Y-m-d H:i:s');
+    }
+
+    /**
+     * The Originator uses this method when restoring its state.
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * The rest of the methods are used by the Caretaker to display metadata.
+     */
+    public function getName(): string
+    {
+        return $this->date . " / (" . substr($this->state, 0, 9) . "...)";
+    }
+
+    public function getDate(): string
+    {
+        return $this->date;
+    }
+}
+
+/**
+ * The Caretaker doesn't depend on the Concrete Memento class. Therefore, it
+ * doesn't have access to the originator's state, stored inside the memento. It
+ * works with all mementos via the base Memento interface.
+ */
+class Caretaker
+{
+    /**
+     * @var Memento[]
+     */
+    private $mementos = [];
+
+    /**
+     * @var Originator
+     */
+    private $originator;
+
+    public function __construct(Originator $originator)
+    {
+        $this->originator = $originator;
+    }
+
+    public function backup(): void
+    {
+        echo "<br/>Caretaker: Saving Originator's state...<br/>";
+        $this->mementos[] = $this->originator->save();
+    }
+
+    public function undo(): void
+    {
+        if (!count($this->mementos)) {
+            return;
+        }
+        $memento = array_pop($this->mementos);
+
+        echo "Caretaker: Restoring state to: " . $memento->getName() . "<br/>";
+        try {
+            $this->originator->restore($memento);
+        } catch (\Exception $e) {
+            $this->undo();
+        }
+    }
+
+    public function showHistory(): void
+    {
+        echo "Caretaker: Here's the list of mementos:<br/>";
+        foreach ($this->mementos as $memento) {
+            echo $memento->getName() . "<br/>";
+        }
+    }
+}
+
+/**
+ * Client code.
+ */
+$originator = new Originator("Super-duper-super-puper-super.");
+$caretaker = new Caretaker($originator);
+
+$caretaker->backup();
+$originator->doSomething();
+
+$caretaker->backup();
+$originator->doSomething();
+
+$caretaker->backup();
+$originator->doSomething();
+
+echo "<br/>";
+$caretaker->showHistory();
+
+echo "<br/>Client: Now, let's rollback!<br/><br/>";
+$caretaker->undo();
+
+echo "<br/>Client: Once more!<br/><br/>";
+$caretaker->undo();
